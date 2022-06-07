4e88f7442333af0c6e22efda3bd208ca1af60ffc
Tyson Lander <tlander@insiderealestate.com>
2022-06-07 17:18:49 -0600

    added react typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+     <Header title="Hello World" color='red'/>
+    </div>
+  );
+}
+
+export default App;
